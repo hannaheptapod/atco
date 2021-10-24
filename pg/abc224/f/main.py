a8f898d53211647ed3e57913e1634c193c29ef39
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2021-10-24 11:31:06 +0900

    ABC224

--- pg/abc224/f/main.py
+++ pg/abc224/f/main.py
@@ -1,3 +1,22 @@
 s = input()
+l = len(s)
 mod = 998244353
+
+def pow2(i):
+    res = 1
+    dos = 2
+    while i:
+        if i % 2: res = res*dos % mod
+        dos = dos**2 % mod
+        i >>= 1
+    return res
+
+ev = [0]*l
+ev[0] = pow2(l - 1) % mod
+for i in range(1, l): ev[i] = (ev[i - 1]*5 + pow2(l - 2)) % mod
+
 ans = 0
+for i in range(l):
+    x = int(s[l - 1 - i])
+    ans = (ans + x*ev[i]) % mod
+print(ans)
