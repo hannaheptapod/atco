3c815c637c4afae6a6f098f8ab296c3707328ec1
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2021-10-31 15:28:15 +0900

    ABC225

--- pg/abc225/d/main.py
+++ pg/abc225/d/main.py
@@ -1,25 +1,26 @@
-
+from collections import deque
 n, q = map(int, input().split())
-g = [[-1, -1]] + [[0, 0] for _ in range(n)]
-def get_first(i):
-    if g[i][0]: return get_first(g[i][0])
-    return i
+g = [[0, 0] for _ in range(n)]
 for _ in range(q):
     qi = list(map(int, input().split()))
     if qi[0] == 1:
-        g[qi[1]][1] = qi[2]
-        g[qi[2]][0] = qi[1]
+        g[qi[1]-1][1] = qi[2]
+        g[qi[2]-1][0] = qi[1]
     elif qi[0] == 2:
-        g[qi[1]][1] = 0
-        g[qi[2]][0] = 0
+        g[qi[1]-1][1] = 0
+        g[qi[2]-1][0] = 0
     else:
-        f = get_first(qi[1])
-        ans = ''
-        now = f
-        cnt = 0
-        while True:
+        x = qi[1]
+        cnt = 1
+        tran = deque([str(x)])
+        now = x
+        while g[now-1][0]:
             cnt += 1
-            ans += ' ' + str(now)
-            if g[now][1]: now = g[now][1]
-            else: break
-        print(str(cnt) + ans)
+            tran.appendleft(str(g[now-1][0]))
+            now = g[now-1][0]
+        now = x
+        while g[now-1][1]:
+            cnt += 1
+            tran.append(str(g[now-1][1]))
+            now = g[now-1][1]
+        print(cnt, ' '.join(tran))
--- pg/abc225/e/main1.py
+++ pg/abc225/e/main1.py
@@ -3,9 +3,8 @@ n = int(input())
 inter = [[0, 0] for _ in range(n)]
 for i in range(n):
     x, y = map(Decimal, input().split())
-    inter[i][0] = (y-1)/x
-    if x > 1: inter[i][1] = y/(x-1)
-    else: inter[i][1] = 10**10
+    inter[i][0] = (y-1) / (x**2 + (y-1)**2)**Decimal(1/2)
+    inter[i][1] = y / ((x-1)**2 + y**2)**Decimal(1/2)
 inter.sort(key=lambda x: x[1])
 end = 0
 ans = 0
