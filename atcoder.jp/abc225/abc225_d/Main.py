16fb46b32c9fd872f68a345ba13a2e25685c4309
Jinks <jin_kishimoto@moegi.waseda.jp>
2021-10-31 15:20:16 +0900

    [AC] abc225 abc225_d

---
+++ atcoder.jp/abc225/abc225_d/Main.py
@@ -0,0 +1,30 @@
+from collections import deque
+n, q = map(int, input().split())
+g = [[0, 0] for _ in range(n)]
+tran = deque()
+for _ in range(q):
+    qi = list(map(int, input().split()))
+    if qi[0] == 1:
+        g[qi[1]-1][1] = qi[2]
+        g[qi[2]-1][0] = qi[1]
+    elif qi[0] == 2:
+        g[qi[1]-1][1] = 0
+        g[qi[2]-1][0] = 0
+    else:
+        x = qi[1]
+        cnt = 1
+        tran.append(str(x))
+        now = x
+        while g[now-1][0]:
+            cnt += 1
+            tran.appendleft(str(g[now-1][0]))
+            now = g[now-1][0]
+        now = x
+        while g[now-1][1]:
+            cnt += 1
+            tran.append(str(g[now-1][1]))
+            now = g[now-1][1]
+        ans = []
+        while tran:
+            ans.append(tran.popleft())
+        print(cnt, ' '.join(ans))
