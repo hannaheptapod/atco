28bf9baeb7fb09078625a1ced91c6e643c2859d4
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2022-02-10 10:58:59 +0900

    Medium 100

--- pg/agc035/a/main.py
+++ pg/agc035/a/main.py
@@ -1,2 +1,21 @@
 n = int(input())
 a = list(map(int, input().split()))
+
+x = [[], []]
+for ai in a:
+    if ai not in x[0]:
+        x[0] += [ai]
+        x[1] += [0]
+
+    x[1][x[0].index(ai)] += 1
+    
+    if len(x[0]) <= 3: continue
+    ans = 'No'
+    break
+else:
+    if len(x[0]) == 1 and x[0][0] == 0: ans = 'Yes'
+    elif len(x[0]) == 2 and 0 in x[0] and 3*x[1][x[0].index(0)] == n: ans = 'Yes'
+    elif len(x[0]) == 3 and x[0][0]^x[0][1]^x[0][2] == 0 and all([3*x1 == n for x1 in x[1]]): ans = 'Yes'
+    else: ans = 'No'
+
+print(ans)
--- pg/caddi2018/c/main.py
+++ pg/caddi2018/c/main.py
@@ -1,13 +1,17 @@
 n, p = map(int, input().split())
+
 def factorization(n):
     arr = []
     temp = n
+
     for i in range(2, int(-(-n**0.5//1)) + 1):
         if temp%i == 0:
             cnt = 0
+
             while temp%i == 0:
                 cnt += 1
                 temp //= i
+
             arr.append([i, cnt])
 
     if temp != 1:
@@ -17,8 +21,10 @@ def factorization(n):
         arr.append([n, 1])
 
     return arr
+
+ans = 1
 f = factorization(p)
-g = 1
-for fi in f:
-    if fi[1] >= n: g *= fi[0]
-print(g)
+
+for fi in f: ans *= fi[0]**(fi[1]//n)
+
+print(ans)
