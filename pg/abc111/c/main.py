931a4d99c6d427625d702c8755569ed0e7127f9a
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2022-02-13 11:36:27 +0900

    Medium 100

--- pg/abc111/c/main.py
+++ pg/abc111/c/main.py
@@ -1,19 +1,11 @@
+from collections import Counter
+
 n = int(input())
 v = list(map(int, input().split()))
 
-d = [{}, {}]
+c1, c2 = [Counter(v[i::2]).most_common(2)+[(0, 0)] for i in range(2)]
 
-for i in range(n):
-    if v[i] not in d[i%2]: d[i%2][v[i]] = 0
-    d[i%2][v[i]] += 1
-
-l = [[[0, 0]], [[0, 0]]]
-for i in d[0].keys(): l[0] += [[d[0][i], i]]
-for i in d[1].keys(): l[1] += [[d[1][i], i]]
-l[0] = sorted(l[0], reverse=True)
-l[1] = sorted(l[1], reverse=True)
-
-if l[0][0][1] != l[1][0][1]: ans = n-(l[0][0][0] + l[1][0][0])
-else: ans = n-max(l[0][0][0] + l[1][1][0], l[0][1][0], l[1][0][0])
+if c1[0][0] != c2[0][0]: ans = n - (c1[0][1]+c2[0][1])
+else: ans = n - max(c1[0][1]+c2[1][1], c1[1][1]+c2[0][1])
 
 print(ans)
