643d6a392e67a1a9451e9757c6990f02f3d8fef1
Jinks <jin_kishimoto@moegi.waseda.jp>
2022-07-02 21:18:29 +0900

    [AC] abc258 abc258_c

---
+++ atcoder.jp/abc258/abc258_c/Main.py
@@ -0,0 +1,9 @@
+N, Q = map(int, input().split())
+S = input()
+
+head = 0
+for _ in range(Q):
+    t, x = map(int, input().split())
+
+    if t == 1: head = (head-x)%N
+    else: print(S[(head+x-1)%N])
