09aa9a7a5814c3b181265bcf5dbbbdca9f01dda5
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2022-09-26 23:05:52 +0900

    AC filling.

---
+++ pg/abc170/d/main.py
@@ -0,0 +1,19 @@
+from collections import Counter, defaultdict
+
+
+def main():
+    _ = int(input())
+    A = sorted(map(int, input().split()))
+
+    cnt = Counter(A)
+    dic = defaultdict(lambda: True)
+
+    for i in cnt:
+        dic[i] &= cnt[i] == 1
+        for j in range(2*i, A[-1]+1, i): dic[j] = False
+
+    ans = sum([1 for a in A if dic[a]])
+    print(ans)
+
+
+if __name__ == '__main__': main()
