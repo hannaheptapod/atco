25537970c82d8d769bb7d06c7529cd5f6baf6379
Jinks <jin_kishimoto@moegi.waseda.jp>
2022-09-27 13:01:39 +0900

    [AC] abc231 abc231_d

---
+++ atcoder.jp/abc231/abc231_d/Main.py
@@ -0,0 +1,57 @@
+from collections import Counter
+
+
+class DSU:
+    def __init__(self, n):
+        self._n = n
+        self.parent_or_size = [-1] * n
+
+    def merge(self, a, b):
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+        x, y = self.leader(a), self.leader(b)
+        if x == y: return x
+        if -self.parent_or_size[x] < -self.parent_or_size[y]: x, y = y, x
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+        return x
+
+    def same(self, a, b):
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a):
+        assert 0 <= a < self._n
+        if self.parent_or_size[a] < 0: return a
+        self.parent_or_size[a] = self.leader(self.parent_or_size[a])
+        return self.parent_or_size[a]
+
+    def size(self, a):
+        assert 0 <= a < self._n
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self):
+        leader_buf = [self.leader(i) for i in range(self._n)]
+        result = [[] for _ in range(self._n)]
+        for i in range(self._n): result[leader_buf[i]].append(i)
+        return [r for r in result if r != []]
+
+
+def main():
+    N, M = map(int, input().split())
+    cnt, dsu = Counter(), DSU(N)
+    ans = 'No'
+    for _ in range(M):
+        a, b = map(lambda x: int(x)-1, input().split())
+        if max(cnt[a], cnt[b]) >= 2: break
+        cnt[a] += 1
+        cnt[b] += 1
+        if dsu.same(a, b): break
+        dsu.merge(a, b)
+    else: ans = 'Yes'
+
+    print(ans)
+
+
+if __name__ == '__main__': main()
