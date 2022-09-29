168d419083316264261eb1431b32b9f7ed1e8152
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2022-09-29 21:21:07 +0900

    AC fillng.

--- pg/arc132/b/main.py
+++ pg/arc132/b/main.py
@@ -1,4 +1,12 @@
-n = int(input())
-p = list(map(int, input().split()))
+def main():
+    N = int(input())
+    P = list(map(int, input().split()))
+
+    diff = P[1]-P[0]
+    if diff == 1 or diff == 1-N: ans = min((N+1-P[0])%N, P[-1]%N+2)
+    else: ans = min(P[0]%N+1, (N+1-P[-1])%N+1)
+
+    print(ans)
 
 
+if __name__ == '__main__': main()
