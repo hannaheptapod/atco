68581ba1242412eb2944484d984db39ebc993e7d
hannaheptapod <jin_kishimoto@moegi.waseda.jp>
2022-10-04 21:47:08 +0900

    [AC] abc188 abc188_e

---
+++ atcoder.jp/abc188/abc188_e/Main.py
@@ -0,0 +1,23 @@
+def main():
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    G = [set() for _ in range(N)]
+    for _ in range(M):
+        x, y = map(lambda x: int(x)-1, input().split())
+        G[x].add(y)
+
+    ans = solve(N, A, G)
+    print(ans)
+
+
+def solve(N, A, G):
+    dp = [float("inf")]*N
+
+    for i in range(N):
+        tmp = min(A[i], dp[i])
+        for j in G[i]: dp[j] = min(dp[j], tmp)
+
+    return max(A[i]-dp[i] for i in range(N))
+
+
+if __name__ == '__main__': main()
