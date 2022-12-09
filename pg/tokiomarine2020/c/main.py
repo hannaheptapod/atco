ed82f3bde21d278c4bc16b0d084680f57a478f4c
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2022-12-09 13:24:08 +0900

    AC filling

---
+++ pg/tokiomarine2020/c/main.py
@@ -0,0 +1,23 @@
+from math import log2
+
+
+def main():
+    N, K = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    for _ in range(min(K, 50)):
+        diff = [0]*N
+
+        for i, ai in enumerate(arr):
+            l, r = max(0, i-ai), min(N-1, i+ai)
+            diff[l] += 1
+            if r+1 < N: diff[r+1] -= 1
+
+        for i in range(1, N): diff[i] += diff[i-1]
+
+        arr = diff[:]
+
+    print(*arr)
+
+
+if __name__ == '__main__': main()
